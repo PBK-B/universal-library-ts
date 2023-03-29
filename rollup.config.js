41adbfb74b4ccd287ca4d44a9382b653a042c35f
PBK-B <PBK-B@PBK6.cn>
2023-03-29 10:13:53 +0800

    feat: init project

---
+++ rollup.config.js
@@ -0,0 +1,85 @@
+/*
+ * @Author: Bin
+ * @Date: 2023-04-01
+ * @FilePath: /universal-library-ts/rollup.config.js
+ */
+import path from 'node:path'
+import glob from 'glob'
+import { URL, fileURLToPath } from 'node:url'
+import process from 'node:process'
+
+import nodeResolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+
+const name = 'UniversalLibrary'
+const fileName = 'index'
+const umdInput = 'src/index.ts'
+const outputDir = 'dist/'
+let sourcemap = false
+let isProd = true
+const inputList = Object.fromEntries(
+    glob.sync('src/**/*.ts').map(file => [path.relative('src', file.slice(0, file.length - path.extname(file).length)), fileURLToPath(new URL(file, import.meta.url))])
+)
+
+if (process.env.ENV === 'development') {
+    // 开发模式时
+    isProd = false
+    sourcemap = true
+}
+
+const getPlugins = ({ isProd = true } = {}) => {
+    const plugins = [
+        nodeResolve(),
+        commonjs(),
+        typescript({ tsconfig: './tsconfig.json' }),
+    ]
+    return plugins
+}
+
+
+// ES module file
+const esmConfig = {
+    input: inputList,
+    treeshake: false,
+    plugins: getPlugins({
+        isProd,
+    }),
+    output: {
+        format: 'esm',
+        sourcemap,
+        chunkFileNames: '_chunks/dep-[hash].js',
+        dir: outputDir,
+    },
+}
+
+// Universal Module
+const umdConfig = {
+    input: umdInput,
+    plugins: getPlugins({
+        isProd,
+    }),
+    output: {
+        name,
+        format: 'umd',
+        exports: 'named',
+        sourcemap,
+        file: `${outputDir}${fileName}.umd.js`,
+    },
+}
+
+const umdMinConfig = {
+    input: umdInput,
+    plugins: getPlugins({
+        isProd: true,
+    }),
+    output: {
+        name,
+        format: 'umd',
+        exports: 'named',
+        sourcemap,
+        file: `${outputDir}${fileName}.umd.min.js`,
+    },
+}
+
+export default [esmConfig, umdConfig, umdMinConfig]
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+/*
+ * @Author: Bin
+ * @Date: 2023-04-01
+ * @FilePath: /universal-library-ts/src/index.ts
+ */
+
+/**
+ * 打招呼函数
+ * @param {string} name
+ * @return {void}
+ */
+function hello(name: string): void {
+    console.log("hello,", name);
+}
+
+export { hello }
